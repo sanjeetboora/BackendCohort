09293d1dc229f1dc8986be83d215d87dbd01308a
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-02-11 19:53:09 +0530

    Create jsObjectAndJSON.js

---
+++ Class8/jsObjectAndJSON.js
@@ -0,0 +1,26 @@
+//js object
+let person ={
+    age: 10,
+    name : "Ganesh",
+    speak: function(){
+        console.log("I can speak");
+    },
+    siblings: ["abc", "def"],
+    marks: {
+        hindi: 23,
+        english: 45
+    }
+}
+
+//json
+//key -> "string"
+//valye -> strings, numbers, objects, array, boolean, null
+let data = {
+    "age" : 10,
+    "name": "Ajit",
+    "siblings" : ["abc", "def"],
+    "marks": {
+        "hindi": 23,
+        "english": 45
+    }
+}
