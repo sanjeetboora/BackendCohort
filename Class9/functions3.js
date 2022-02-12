f0cb68944a751ac8ec1edc30abdcd961bbed3754
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-02-12 08:57:46 +0530

    Create functions3.js

---
+++ Class9/functions3.js
@@ -0,0 +1,20 @@
+function greet(name){
+    console.log("hello", name);
+}
+greet("Manisha");
+
+//arrow function
+
+greet1 = (name) => {
+    console.log("hello", name);
+}
+greet1("madhu");
+
+
+greet2 = (name) => console.log("hello", name);
+greet2("sarang");
+
+sum = (a, b) => a+b;
+
+let result = sum(2, 3);
+console.log(result);
