6e51f99f4ebf323c3e9cd7d46b89ca76ef6c10d1
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-05-06 23:16:55 +0530

    code added

--- Stack/stack.js
+++ Stack/stack.js
@@ -34,6 +34,28 @@ class Stack{
         }
         return this.data[this.top];
     }
+
+    insertElementAtBottom(value){
+        if(this.isEmpty()){//base case
+            this.push(value);
+            return;
+        }
+
+        let x = this.peek();
+        this.pop();
+        this.insertElementAtBottom(value);
+        this.push(x);
+    }
+
+    reverse(){
+        if(this.isEmpty()){
+            return;
+        }
+        let temp = this.peek();
+        this.pop();
+        this.reverse();
+        this.insertElementAtBottom(temp);
+    }
 }
 
 let myStack = new Stack();
@@ -44,18 +66,18 @@ myStack.push(20);
 myStack.push(30);
 //console.log(myStack.peek());
 
-console.log(myStack.peek());
+// console.log(myStack.peek());
 myStack.pop();
-console.log(myStack.peek());
+// console.log(myStack.peek());
 myStack.pop();
-console.log(myStack.peek());
+// console.log(myStack.peek());
 myStack.push(100);
-console.log(myStack.peek());
+// console.log(myStack.peek());
 myStack.pop();
-console.log("size", myStack.size());
-console.log(myStack.peek());
+// console.log("size", myStack.size());
+// console.log(myStack.peek());
 myStack.pop();
-console.log("size", myStack.size());
+//console.log("size", myStack.size());
 if(!myStack.isEmpty()){
     myStack.pop();
     console.log(myStack.peek());
@@ -65,6 +87,65 @@ myStack.push(200);
 myStack.push(201);
 myStack.push(202);
 myStack.push(203);
-myStack.push(204);
+//myStack.push(204);
 
-myStack.push(205);
+// myStack.push(205);
+
+//console.log(myStack.peek());
+//console.log("size",myStack.size());
+myStack.insertElementAtBottom(500);
+
+myStack.reverse();
+console.log(myStack.peek());
+//console.log("size",myStack.size());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+console.log(myStack.peek());
+myStack.pop();
+//console.log(myStack.peek());
+
+
+
+function checkParentheses(str){
+    const st = new Stack();
+
+    for(const ch of str){
+        if(ch == '(' || ch == '{' || ch == '['){
+            st.push(ch);
+        }else if(ch == ')'){
+            if(st.peek() == '('){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        else if(ch == ']'){
+            if(st.peek() == '['){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+            
+        }
+        else if(ch == '}'){
+            if(st.peek() == '{'){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    return st.isEmpty();
+}
+
+console.log(checkParentheses("[{()}{}]"));
+console.log(checkParentheses("[{(}}{}]"));
