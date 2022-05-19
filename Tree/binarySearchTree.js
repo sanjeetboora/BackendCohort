751bd2b0ecfff4df81dc3d8afe1cbcf5fe0b0cb9
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-05-19 16:58:27 +0530

    code added

---
+++ Tree/binarySearchTree.js
@@ -0,0 +1,91 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function insertInBST(root, data){
+    if(root == null){ //base case
+        root = new Node(data);
+        return root;
+    }
+
+    if(data < root.data){
+        root.left = insertInBST(root.left, data);
+    }
+    else{
+        root.right = insertInBST(root.right, data);
+    }
+    return root;
+}
+
+function minValue(root){
+    if(root == null){
+        return NaN;
+    }
+    while(root.left != null){
+        root = root.left;
+    }
+    return root.data;
+}
+
+function deleteInBST(root, data){
+    if(root == null){
+        return;
+    }
+    if(data < root.data){
+        root.left = deleteInBST(root.left, data);
+    }
+    else if(data > root.data){
+        root.right = deleteInBST(root.right, data);
+    }
+    //found the node to delete
+    else if(root.left != null && root.right != null){ //node have 2 child
+        root.data = minValue(root.right); //inorder successor
+        root.right = deleteInBST(root.right, root.data);
+    }
+    else if(root.left != null || root.right != null){//node have 1 child
+        root = root.left ||root.right;
+    }
+    else{//node have 0 child => leaf node
+        root = null;
+    }
+
+    return root;
+}
+
+
+function inorderTraversal(root){
+    if(root == null) return; //base case
+    // left ,root, right
+    inorderTraversal(root.left);
+    console.log(root.data);
+    inorderTraversal(root.right);
+}
+
+let root = new Node(10);
+// inorderTraversal(root);
+// console.log("--------------------");
+insertInBST(root, 100);
+// inorderTraversal(root);
+// console.log("--------------------");
+insertInBST(root, 5);
+// inorderTraversal(root);
+// console.log("--------------------");
+insertInBST(root, 15);
+// inorderTraversal(root);
+// console.log("--------------------");
+insertInBST(root, 1);
+inorderTraversal(root);
+console.log("--------------------");
+deleteInBST(root, 5);
+inorderTraversal(root);
+console.log("--------------------");
+deleteInBST(root, 10);
+inorderTraversal(root);
+console.log("--------------------");
+deleteInBST(root, 15);
+inorderTraversal(root);
+console.log("--------------------");
--- Tree/binaryTree.js
+++ Tree/binaryTree.js
@@ -29,7 +29,6 @@ function inorderTraversal(root){
     inorderTraversal(root.left);
     console.log(root.data);
     inorderTraversal(root.right);
-    
 }
 
 
@@ -52,3 +51,28 @@ inorderTraversal(root);
 //     /  \     \
 //    40  50     60
 console.log(root);
+
+
+function buildTree(input, n){
+    let arr = [];
+    let parentIndex=0;
+    arr[parentIndex] = input[0];
+    for(let i=1; i<n; i=i+2){
+        let left= 2*parentIndex +1;
+        let right= 2*parentIndex +2;
+        arr[left] = input[i];
+        arr[right] = input[i+1];
+        parentIndex++;
+    }
+    return arr;
+}
+
+let input = [10, 20, 30, 40, 50, null, 60, null, null, null, null, null, null, null, null];
+
+console.log(buildTree(input, 15));
+//         10
+//        /  \
+//      20    30
+//     /  \     \
+//    40  50     60
+
