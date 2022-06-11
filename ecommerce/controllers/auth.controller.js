4a0afd910ba03b5488a75ab7be3568ead4e0c1e8
Shetty-Nithin <62413993+Shetty-Nithin@users.noreply.github.com>
2022-06-11 16:51:29 +0530

    Update auth.controller.js
    added "[]" at linne number 24 for "[req.body.roles]"

--- ecommerce/controllers/auth.controller.js
+++ ecommerce/controllers/auth.controller.js
@@ -21,7 +21,7 @@ exports.signup = (req, res) => {
             Role.findAll({
                 where: {
                     name: {
-                        [Op.or]: req.body.roles
+                        [Op.or]: [req.body.roles]
                     }
                 }
             }).then(roles => {
